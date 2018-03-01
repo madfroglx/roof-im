684cfd31a8b07d560e20781af92702610c861f60
liuxin <madfrogxin@163.com>
2018-03-01 20:55:29 +0800

    Jdbc message dao

--- roof-im-service/src/main/java/org/roof/im/message/MessageDao.java
+++ roof-im-service/src/main/java/org/roof/im/message/MessageDao.java
@@ -1,5 +1,8 @@
 package org.roof.im.message;
 
+import java.util.Date;
+import java.util.List;
+
 public interface MessageDao {
     /**
      * 保存消息
@@ -15,5 +18,25 @@ public interface MessageDao {
      * @param state     当前状态
      */
     int updateState(Long messageId, int state);
+
+    /**
+     * 查询最近的记录
+     *
+     * @param limit 查询数量
+     * @return 记录列表
+     */
+    List<Message> queryLatest(String receiver, int limit);
+
+    /**
+     * 查询消息
+     *
+     * @param receiver 用户名
+     * @param start    消息发送开始时间
+     * @param end      消息发送结束时间
+     * @param offset   记录偏移
+     * @param limit    查询数量
+     * @return 记录列表
+     */
+    List<Message> query(String receiver, Date start, Date end, int offset, int limit);
 
 }
--- roof-im-service/src/main/java/org/roof/im/message/jdbc/JdbcMessageDao.java
+++ roof-im-service/src/main/java/org/roof/im/message/jdbc/JdbcMessageDao.java
@@ -6,13 +6,19 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.JdbcTemplate;
 import org.springframework.stereotype.Component;
 
+import java.util.Date;
+import java.util.List;
+
 @Component
 public class JdbcMessageDao implements MessageDao {
-    private static final String MESSAGE_SAVE_SQL = "insert into im_message(sender,receiver,type,payload,create_time,state) " +
+    private static final String MESSAGE_SAVE_SQL = "insert into im_message˚(sender,receiver,type,payload,create_time,state) " +
             "values(?, ?, ?, ?, ?, ?)";
     private static final String LAST_INSERT_ID = "select LAST_INSERT_ID()";
-
     private static final String MESSAGE_STATE_UPDATE_SQL = "update im_message set state = ? where id = ?";
+    private static final String QUERY_LATEST_SQL = "select * from im_message where receiver = ? order by create_time desc limit ?";
+    private static final String QUERY_SQL = "select * from im_message where receiver = ? and create_time > ? and create_time < ? " +
+            " order by create_time desc limit ?,?";
+
     private JdbcTemplate jdbcTemplate;
 
     @Override
@@ -26,6 +32,16 @@ public class JdbcMessageDao implements MessageDao {
     @Override
     public int updateState(Long messageId, int state) {
         return jdbcTemplate.update(MESSAGE_STATE_UPDATE_SQL, state, messageId);
+    }
+
+    @Override
+    public List<Message> queryLatest(String receiver, int limit) {
+        return jdbcTemplate.queryForList(QUERY_LATEST_SQL, new Object[]{receiver, limit}, Message.class);
+    }
+
+    @Override
+    public List<Message> query(String receiver, Date start, Date end, int offset, int limit) {
+        return jdbcTemplate.queryForList(QUERY_SQL, new Object[]{receiver, start.getTime(), end.getTime(), offset, limit}, Message.class);
     }
 
     @Autowired
