cd9a1abe5e3ae3aba4b8fbf4bcd5b3dc503a8c73
hzliuxin1 <hzliuxin1@corp.netease.com>
2018-03-06 12:55:17 +0800

    redis订阅

---
+++ roof-im-service/src/main/java/org/roof/im/transport/redis/RedisBlockingQueueRequestMessageDriveEnterPoint.java
@@ -0,0 +1,14 @@
+package org.roof.im.transport.redis;
+
+import org.roof.im.transport.queue.AbstractBlockingQueueRequestMessageDriveEnterPoint;
+
+/**
+ * @author hzliuxin1
+ * @since 2018/3/6 0006
+ */
+public class RedisBlockingQueueRequestMessageDriveEnterPoint<E> extends AbstractBlockingQueueRequestMessageDriveEnterPoint {
+    @Override
+    protected void onInit() {
+
+    }
+}
