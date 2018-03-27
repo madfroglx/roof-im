2abffe1b06ac69cecb62d9a39a6cba73612f189b
liuxin <madfrogxin@163.com>
2018-03-27 22:05:16 +0800

    消息接口返回完整消息体

--- roof-im-service/src/main/java/org/roof/im/chain/handler/impl/MessageHandlerNode.java
+++ roof-im-service/src/main/java/org/roof/im/chain/handler/impl/MessageHandlerNode.java
@@ -1,17 +1,21 @@
 package org.roof.im.chain.handler.impl;
 
 import com.roof.chain.api.ValueStack;
+import com.roof.chain.support.NodeResult;
 import org.roof.im.chain.ImConstant;
 import org.roof.im.chain.handler.AbstractRequestHandlerNode;
+import org.roof.im.message.Message;
 import org.roof.im.request.MessageRequest;
+import org.roof.im.user.UserService;
 import org.roof.im.user.UserState;
+import org.roof.im.user.UserStateService;
 
 import java.util.List;
 
 /**
  * 消息处理
  */
-public class MessageHandlerNode extends AbstractRequestHandlerNode<MessageRequest> {
+public class MessageHandlerNode {
     /**
      * 消息接受者不存在
      */
@@ -25,17 +29,29 @@ public class MessageHandlerNode extends AbstractRequestHandlerNode<MessageReques
      */
     private static final String MESSAGE_HANDLE_SUCCESS = "messageHandleSuccess";
 
-    public String doNode(MessageRequest request, ValueStack valueStack) {
+    private UserService userService;
+    private UserStateService userStateService;
+
+    public NodeResult doNode(MessageRequest request, Message message, ValueStack valueStack) {
         String receiver = request.getReceiver();
         if (!userService.exist(receiver)) {
-            return RECEIVER_NOT_EXISTS;
+            return new NodeResult(RECEIVER_NOT_EXISTS);
         }
         List<UserState> states = userStateService.getStates(receiver);
         if (states == null || states.size() == 0) {
-            return RECEIVER_OFFLINE;
+            NodeResult r = new NodeResult(RECEIVER_OFFLINE);
+            r.setData(message);
+            return r;
         }
         valueStack.set(ImConstant.USER_STATES, states);
-        return MESSAGE_HANDLE_SUCCESS;
+        return new NodeResult(MESSAGE_HANDLE_SUCCESS);
     }
 
+    public void setUserService(UserService userService) {
+        this.userService = userService;
+    }
+
+    public void setUserStateService(UserStateService userStateService) {
+        this.userStateService = userStateService;
+    }
 }
